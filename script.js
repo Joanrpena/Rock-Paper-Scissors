47968876c531a89d0befba99aaccc47d585147a1
Joan Pena <joan.r.pena1@gmail.com>
2023-06-26 00:10:41 -0400

    Added getPlayerSelection function, and getComputerSelection function. Able to fully play a round of Rock Paper Scissors

--- script.js
+++ script.js
@@ -6,13 +6,69 @@
 //Create an array containing all 3 choices
 //Create Function getComputerChoice()
 //Use Math.floor and Math.random function to select random choice from array
-//arr[math.floor(math.random() * array length)]
+//choices[math.floor(math.random() * array length)]
 
-let arr = ['Rock','Paper','Scissors'];
+const choices = ['rock','paper','scissors'];
 
-function getComputerChoice(){
-
-    console.log(arr[Math.floor(Math.random() * 3)]);
-
+function getComputerChoice()
+{
+    return choices[Math.floor(Math.random() * choices.length)];
 }
-getComputerChoice();
+
+
+function getPlayerChoice(){
+    let choice = prompt('Pick your choice!').toLowerCase();
+
+   if (choice == 'rock')
+    {
+        return 'rock';
+} 
+else if (choice == 'paper'){
+    return 'paper';
+}
+else if (choice == 'scissors'){
+    return 'scissors';
+}else{
+    return 'Not a valid choice';
+}
+}
+
+// Write a function that plays a single round
+// Create playerSelection parameter (case sensitive)
+// Function for playing round requires 2 parameters playerSelection and computerSelection
+//Return string that declares winner
+
+
+let playerPoint = 0;
+
+function playRound(){
+    let playerSelection = getPlayerChoice();
+    let computerSelection = getComputerChoice();
+    
+    
+    if ((playerSelection == 'rock' && computerSelection == 'scissors')||
+        (playerSelection == 'scissors' && computerSelection == 'paper') ||
+        (playerSelection == 'paper' && computerSelection == 'rock')) 
+        
+        {
+            playerPoint ++;
+            console.log(`You win! ${playerSelection.toUpperCase()} beats ${computerSelection.toUpperCase()}`);
+            return playerPoint;
+        } 
+        else if (playerSelection == computerSelection){
+            console.log("Its a tie!!");
+        } else if ((playerSelection != 'rock') ||
+                   (playerSelection != 'paper') ||
+                   (playerSelection != 'scissors')){
+                    return 'Enter a valid choice';
+                   }else {
+
+            console.log(`You lose! ${computerSelection.toUpperCase()} beats ${playerSelection.toUpperCase()}`);
+        }
+   
+ }
+
+console.log(playRound());
+console.log(playerPoint);
+
+
